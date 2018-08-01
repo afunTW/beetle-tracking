7561b2a5e1492a6e653aa7d1c7322a82a8841492
afunTW <endlessproof@gmail.com>
2018-08-01 10:40:03 +0800

    check python version with pyenv and download

--- setup.bash
+++ setup.bash
@@ -3,6 +3,17 @@
 GITHUB_DETECTION="https://github.com/afunTW/TensorBox"
 GITHUB_CLASSIFICATION="https://github.com/afunTW/keras-transfer-workflow"
 
+# check the env for pyenv if the system do not have the right version of Pipfile
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.profile
+source ~/.profile
+
 # check detection and classfication site-project
 if [ ! -d "detection" ]; then
 	echo "Can't find the detection folder, clone from github..."
