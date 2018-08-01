e33177cc6cf6a4725aa2cd42a654ffa2680f80fa
afunTW <endlessproof@gmail.com>
2018-08-01 18:57:17 +0800

    check pyenv and make detection .so

--- setup.bash
+++ setup.bash
@@ -4,11 +4,11 @@ GITHUB_DETECTION="https://github.com/afunTW/TensorBox"
 GITHUB_CLASSIFICATION="https://github.com/afunTW/keras-transfer-workflow"
 
 # check the env for pyenv if the system do not have the right version of Pipfile
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev
+if [ ! -d "$HOME/.pyenv" ]; then
+	sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+	xz-utils tk-dev libffi-dev
 
-if [ ! -d "~/.pyenv" ]; then
 	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
@@ -20,14 +20,12 @@ fi
 if [ ! -d "detection" ]; then
 	echo "Can't find the detection folder, clone from github..."
 	git clone $GITHUB_DETECTION detection
-	cd detection
-	pipenv sync
-	cd ..
+	cd detection && pipenv sync
+	source $(pipenv --venv)/bin/activate
+	cd utils && make && deactivate && cd ../..
 fi
 if [ ! -d "classification" ]; then
 	echo "Can't find the classification folder, clone from github..."
 	git clone $GITHUB_CLASSIFICATION classification
-	cd classification
-	pipenv sync
-	cd ..
+	cd classification && pipenv sync && cd ..
 fi
