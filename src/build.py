cdab8937b50d210dec45cccd4dab6a51ad652dfb
afunTW <endlessproof@gmail.com>
2018-08-14 10:13:30 +0800

    remove unused code

--- src/build.py
+++ src/build.py
@@ -202,7 +202,6 @@ def build_flow(video:str, filename: str, config: str):
             if len(block.bboxes) > config['block_length_threshold'] and \
             block.confidence > config['block_scroe_threshold']:
                 trackflow.append_block(block.label, block)
-                # trackflow.paths[block.label] += block.bboxes
         for k, v in trackflow.blocks.items():
             v = sorted(v, key=lambda x: x.head.frame_idx)
             for block in v:
