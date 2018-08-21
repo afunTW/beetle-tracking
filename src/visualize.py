dbf44e86e6a919b78cb4295664b14134f8c0235c
afunTW <endlessproof@gmail.com>
2018-08-21 17:49:36 +0800

    add tracking, action visualize on video

--- src/visualize.py
+++ src/visualize.py
@@ -10,6 +10,45 @@ from tqdm import tqdm
 
 LOGGER = logging.getLogger(__name__)
 
+def _merge_bbox(pt1_lt, pt1_rb, pt2_lt, pt2_rb):
+    lt = (min(pt1_lt[0], pt2_lt[0]), min(pt1_lt[1], pt2_lt[1]))
+    rb = (max(pt1_rb[0], pt2_rb[0]), max(pt1_rb[1], pt2_rb[1]))
+    return lt, rb
+
+def _get_bbox_message(row, config):
+    show_msg = ''
+    detect_score = float(row['detect_score']) if 'detect_score' in row else None
+    label_col = '{}_score'.format(row['label']) if 'label' in row else None
+    label_score = float(row[label_col]) if label_col and label_col in row else None
+    block_score = float(row['block_score']) if 'block_score' in row else None
+    is_on_mouse = int(row['on_mouse']) if 'on_mouse' in row else None
+    msg_col = row['msg'] if 'msg' in row else None
+    if config.get('show_detect_score', False) and detect_score:
+        show_msg += 'detect {:.2f}'.format(detect_score)
+    if config.get('show_class_score', False) and label_score:
+        show_msg = show_msg + ', ' if show_msg else show_msg
+        show_msg += 'class {:.2f}'.format(label_score)
+    if config.get('show_block_score', False) and block_score:
+        show_msg = show_msg + '\n' if show_msg else show_msg
+        show_msg += 'block {:.2f}'.format(block_score)
+    if config.get('show_msg_on_mouse', False) and is_on_mouse:
+        show_msg = show_msg + '\n' if show_msg else show_msg
+        show_msg += 'on mouse'
+    if config.get('show_msg_column', False) and msg_col:
+        show_msg = show_msg + '\n' if show_msg else show_msg
+        show_msg += msg_col
+    return show_msg
+
+def _get_center(row):
+    col1 = ['center.x', 'center.y']
+    col2 = ['center1.x', 'center1.y', 'center2.x', 'center2.y']
+    is_col1 = [c in row for c in col1]
+    is_col2 = [c in row for c in col2]
+    if all(is_col1):
+        return tuple(int(row[c]) for c in col1)
+    if all(is_col2):
+        return tuple(int(row[c]) for c in col2[:2]), tuple(int(row[c]) for c in col2[2:])
+
 def _get_label_color(label=None):
     color_map = {
         'X': (215, 255, 0),     # Gold
@@ -73,13 +112,87 @@ def _convert_classification(records):
         ])
     return cols, data
 
+def _convert_action(df):
+    results_cols = [
+        'frame_idx', 'pt1.x', 'pt1.y', 'pt2.x', 'pt2.y',
+        'center1.x', 'center1.y', 'center2.x', 'center2.y',
+        'label', 'behavior', 'target_label', 'msg'
+    ]
+    data = []
+    df_action = 'behavior' in df and df.loc[~df.behavior.isna()].copy()
+    df_action['target_label'] = df_action['target'].apply(lambda x: x.split(' ')[-1])
+    for name, group in tqdm(df_action.groupby('frame_idx')):
+        _checked_in_group = []
+        for row_idx, row in group.iterrows():
+            t_row = df.loc[(df.frame_idx == row['frame_idx']) & (df.label == row['target_label'])]
+            if not t_row.empty:
+                # data preprocess and check whether available
+                t_row = t_row.reset_index(drop=True).loc[0,:].fillna('')
+                if t_row['label'] in _checked_in_group:
+                    continue
+
+                # preprare data and append
+                subject_pt1, subject_pt2 = (row['pt1.x'], row['pt1.y']), (row['pt2.x'], row['pt2.y'])
+                target_pt1, target_pt2 = (t_row['pt1.x'], t_row['pt1.y']), (t_row['pt2.x'], t_row['pt2.y'])
+                merge_pt1, merge_pt2 = _merge_bbox(subject_pt1, subject_pt2, target_pt1, target_pt2)
+                subject_center = (row['center.x'], row['center.y'])
+                target_center = (t_row['center.x'], t_row['center.y'])
+
+                # message
+                event_state = row['event_type'].split(' ')[-1]
+                msg = '{} {} {} - {}'.format(row['label'], row['behavior'], row['target_label'], event_state)
+                if 'behavior' in t_row and t_row['behavior']:
+                    target_label = t_row['target'].split(' ')[-1]
+                    event_state = t_row['event_type'].split(' ')[-1]
+                    msg += '\n{} {} {} - {}'.format(t_row['label'], t_row['behavior'], target_label, event_state)
+
+                data.append([
+                    row['frame_idx'], *merge_pt1, *merge_pt2, *subject_center, *target_center,
+                    row['label'], row['behavior'], row['target_label'], msg
+                ])
+                _checked_in_group += [row['label'], row['target_label']]
+
+    return pd.DataFrame(data, columns=results_cols)
+
 def convert_to_dataframe(records, option):
     cols, data = [], None
     if option == 'detection':
         cols, data = _convert_detection(records)
     elif option == 'classification':
         cols, data = _convert_classification(records)
+    elif option == 'action':
+        return _convert_action(records)
     return pd.DataFrame(data, columns=cols)
+
+def draw_mouse_contours(frame, frame_idx, mouse_contours, mouse_idx, current_key):
+    """[summary]
+    
+    Arguments:
+        frame {np.ndarray} -- target image
+        mouse_contours {dict} -- each mouse contours
+        mouse_idx {list} -- list of mouse contours keys
+        current_id {int} -- current mouse contours keys index
+    """
+    next_key = min(mouse_idx.index(current_key)+1, len(mouse_idx)-1)
+    next_key = mouse_idx[next_key]
+    next_key = next_key if next_key != current_key else None
+
+    current_mouse = mouse_contours[current_key]
+    next_mouse = mouse_contours[next_key] if next_key else None
+    mouse = current_mouse
+    
+    if next_mouse and frame_idx >= int(next_key):
+        current_key = next_key
+        next_key = min(mouse_idx.index(next_key)+1, len(mouse_idx)-1)
+        next_key = mouse_idx[next_key]
+        mouse = next_mouse
+
+    for cnt_idx, cnt in enumerate(mouse['contour_extend'][1:]):
+        mouse_color = _get_label_color(label='mouse')
+        pt1, pt2 = tuple(mouse['contour_extend'][cnt_idx-1][0]), tuple(cnt[0])
+        cv2.line(frame, pt1, pt2, mouse_color, 2, cv2.LINE_AA)
+    
+    return current_key, frame
 
 def show_and_save_video(video, records, config,
                         mouse_contours=None,
@@ -88,6 +201,8 @@ def show_and_save_video(video, records, config,
                         save_video=None,
                         pause_flag=False):
     """[summary]
+
+    draw path, label and text to frame based on pd.DataFrame
     
     Arguments:
         video {str} -- video path to read frames
@@ -111,14 +226,11 @@ def show_and_save_video(video, records, config,
         video_writer = cv2.VideoWriter(save_video, fourcc, 15, resolution)
 
     # check mouse
-    current_ref_mouse_idx, next_ref_mouse_idx, mouse_idx = None, None, None
+    current_ref_mouse_idx, mouse_idx = None, None
     if isinstance(mouse_contours, dict):
-        mouse_idx = sorted(mouse_contours.keys(), lambda x: int(x))
+        mouse_idx = sorted(mouse_contours.keys(), key=lambda x: int(x))
         mouse_idx = list(map(str, mouse_idx))
-        if len(mouse_contours.keys()) > 1:
-            current_ref_mouse_idx, next_ref_mouse_idx = mouse_idx[0], mouse_idx[1]
-        elif len(mouse_contours.keys()) == 1:
-            current_ref_mouse_idx = mouse_idx[0]
+        current_ref_mouse_idx = mouse_idx[0]
 
     # draw by each frame
     frame_total_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -139,37 +251,23 @@ def show_and_save_video(video, records, config,
         
         # draw mouse
         if current_ref_mouse_idx:
-            # determine thr reference mouse
-            current_ref_mouse = mouse_contours[current_ref_mouse_idx]
-            next_ref_mouse = mouse_contours[next_ref_mouse_idx] if next_ref_mouse_idx else None
-            mouse = current_ref_mouse
-            
-            if next_ref_mouse:
-                if frame_idx >= next_ref_mouse.frame_idx:
-                    current_ref_mouse_idx = next_ref_mouse_idx
-                    next_ref_mouse_idx = min(len(mouse_idx)-1, next_ref_mouse_idx+1)
-                    mouse = next_ref_mouse
-
-            for cnt_idx, cnt in enumerate(mouse['contour_extend'][1:]):
-                    mouse_color = _get_label_color(label='mouse')
-                    pt1, pt2 = tuple(mouse['contour_extend'][cnt_idx-1][0]), tuple(cnt[0])
-                    cv2.line(frame, pt1, pt2, mouse_color, 2, cv2.LINE_AA)
+            current_ref_mouse_idx, frame = draw_mouse_contours(
+                frame, frame_idx, mouse_contours, mouse_idx, current_ref_mouse_idx
+            )
         
         # draw beetle
         groupby_col = 'block_idx' if 'block_idx' in records else None
         iter_df = (groupby_col and df_candidate.groupby(groupby_col)) or None
         iter_df = [('all', df_candidate)] if iter_df is None else iter_df
-    
-        # draw path
         text_color = (255, 255, 255)
         for name, group in iter_df:
             group.reset_index(drop=True, inplace=True)
             for row_idx, row in group.iterrows():
                 decide_color_col = row['label'] if 'label' in records else None
                 label_color = _get_label_color(decide_color_col)
-                pts = pd.to_numeric(row['pt1.x':'center.y'], downcast='integer')
+                pts = pd.to_numeric(row['pt1.x':'pt2.y'], downcast='integer')
                 pt1, pt2 = (pts['pt1.x'], pts['pt1.y']), (pts['pt2.x'], pts['pt2.y'])
-                center = (pts['center.x'], pts['center.y'])
+                center = _get_center(row)
 
                 # draw start point
                 if config.get('show_start_bbox', False) and row_idx == 0:
@@ -178,36 +276,30 @@ def show_and_save_video(video, records, config,
                 # draw path
                 if row_idx > 0 and groupby_col:
                     prev_row = group.loc[row_idx-1, :]
-                    prev_center = (prev_row['center.x'], prev_row['center.y'])
-                    cv2.line(frame, prev_center, center, label_color, 1, cv2.LINE_AA)
+                    prev_center = _get_center(prev_row)
+                    if len(prev_center) == 1 and len(center) == 1:
+                        cv2.line(frame, prev_center, center, label_color, 1, cv2.LINE_AA)
+                    elif len(prev_center) == 2 and len(center) == 2:
+                        color1 = _get_label_color(row['label'])
+                        color2 = _get_label_color(row['target_label'])
+                        cv2.line(frame, prev_center[0], center[0], color1, 1, cv2.LINE_AA)
+                        cv2.line(frame, prev_center[1], center[1], color2, 1, cv2.LINE_AA)
 
                 if row['frame_idx'] == frame_idx:
                     cond_enable_bbox = config.get('show_highlight_on_mouse', False) and \
-                                        'on_mouse' in group and \
-                                        int(row['on_mouse'])
-                    if 'label' in row:
+                                        'on_mouse' in group and int(row['on_mouse'])
+                    if len(center) == 1 and 'label' in row:
                         cv2.putText(frame, row['label'], center, cv2.FONT_HERSHEY_COMPLEX, 1, label_color, 2)
+                    elif len(center) == 2 and 'label' in row and 'target_label' in row:
+                        color1 = _get_label_color(row['label'])
+                        color2 = _get_label_color(row['target_label'])
+                        cv2.putText(frame, row['label'], center[0], cv2.FONT_HERSHEY_COMPLEX, 1, color1, 2)
+                        cv2.putText(frame, row['target_label'], center[1], cv2.FONT_HERSHEY_COMPLEX, 1, color2, 2)
                     if config.get('show_bbox', False) or cond_enable_bbox:
                         cv2.rectangle(frame, pt1, pt2, label_color, 2)
                     
                     # show message
-                    show_msg = ''
-                    detect_score = float(row['detect_score']) if 'detect_score' in group else None
-                    label_col = '{}_score'.format(row['label']) if 'label' in row else None
-                    label_score = float(row[label_col]) if label_col and label_col in group else None
-                    block_score = float(row['block_score']) if 'block_score' in group else None
-                    is_on_mouse = int(row['on_mouse']) if 'on_mouse' in group else None
-                    if config.get('show_detect_score', False) and detect_score:
-                        show_msg += 'detect {:.2f}'.format(detect_score)
-                    if config.get('show_class_score', False) and label_score:
-                        show_msg = show_msg + ', ' if show_msg else show_msg
-                        show_msg += 'class {:.2f}'.format(label_score)
-                    if config.get('show_block_score', False) and block_score:
-                        show_msg = show_msg + '\n' if show_msg else show_msg
-                        show_msg += 'block {:.2f}'.format(block_score)
-                    if config.get('show_msg_on_mouse', False) and is_on_mouse:
-                        show_msg = show_msg + '\n' if show_msg else show_msg
-                        show_msg += 'on mouse'
+                    show_msg = _get_bbox_message(row, config)
                     if show_msg:
                         for lid, msg in enumerate(show_msg.split('\n')[::-1]):
                             show_x, show_y = pt1
--- visualization.py
+++ visualization.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import logging
+from pathlib import Path
 
 import pandas as pd
 
@@ -14,10 +15,11 @@ def argparser():
     parser.add_argument('-v', '--video', dest='video', required=True)
     parser.add_argument('-o', '--options',
                         dest='options',
-                        choices=['detection', 'classification', 'track'],
+                        choices=['detection', 'classification', 'track', 'action'],
                         required=True)
     parser.add_argument('-i', '--input-record', dest='record', required=True)
     parser.add_argument('-c', '--config', dest='config', default='config/default.json')
+    parser.add_argument('-m', '--mouse', dest='mouse_contours')
     parser.add_argument('--from', dest='from_idx', default=0, type=int)
     parser.add_argument('--show-video', dest='show_video', action='store_true', help='show video with cv2')
     parser.add_argument('--no-show-video', dest='show_video', action='store_false')
@@ -34,10 +36,18 @@ def main(args):
     logger = logging.getLogger(__name__)
     log_handler(logger, logging.getLogger('src.visualize'))
     logger.info(args)
+    save_video_path = Path(args.video).with_name(args.outvideo) if args.save_video else None
 
     with open(args.config, 'r') as f:
         config = json.load(f)['outputs']
-
+    if args.mouse_contours:
+        with open(args.mouse_contours, 'r') as f:
+            mouse_contours = json.load(f)
+    if args.save_video:
+        if not save_video_path.parent.exists():
+            save_video_path.parent.mkdir(parents=True)
+        save_video_path = str(save_video_path)
+    
     if args.options in ['detection', 'classification']:
         with open(args.record, 'r') as f:
             records = f.readlines()
@@ -46,15 +56,25 @@ def main(args):
         show_and_save_video(args.video, df, config,
                             from_=args.from_idx,
                             show_video=args.show_video,
-                            save_video=args.save_video,
+                            save_video=save_video_path,
                             pause_flag=args.pause)
-
     elif args.options == 'track':
         df = pd.read_csv(args.record)
         show_and_save_video(args.video, df, config,
                             from_=args.from_idx,
+                            mouse_contours=mouse_contours,
                             show_video=args.show_video,
-                            save_video=args.save_video,
+                            save_video=save_video_path,
+                            pause_flag=args.pause)
+    elif args.options == 'action':
+        df = pd.read_csv(args.record)
+        if Path(args.record).name != 'fixed_action_paths.csv':
+            df = convert_to_dataframe(df, args.options)
+            df.to_csv(Path(args.record).with_name('fixed_action_paths.csv'))
+        show_and_save_video(args.video, df, config,
+                            from_=args.from_idx,
+                            show_video=args.show_video,
+                            save_video=save_video_path,
                             pause_flag=args.pause)
 
 if __name__ == '__main__':
