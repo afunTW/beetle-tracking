d4fc95ec47fea9efc715164709418ea31b0e12c7
afunTW <endlessproof@gmail.com>
2018-08-29 17:11:23 +0800

    fixed and add the action data generating

--- demo.bash
+++ demo.bash
@@ -2,44 +2,83 @@
 
 DATE=$(date "+%Y%m%d_%H%M%S")
 GPU_ID=$1
-VIDEO_FULLNAME=$2
-OUTPUT_VIDEO_NAME=${3:-"$DATE.avi"}
+VIDEO_FULLNAME=$(realpath $2)
 
 VIDEO_DIRNAME=$(dirname $VIDEO_FULLNAME)
 VIDEO_BASENAME=$(basename $VIDEO_FULLNAME)
-VIDEO_EXT=${VIDEO_BASENAME#*.}
-VIDEO_NAME=${VIDEO_BASENAME%%.*}
+VIDEO_EXT=${VIDEO_BASENAME##*.}
+VIDEO_NAME=${VIDEO_BASENAME%.*}
+
+EXPECT_DETECT_OUTPUT="$VIDEO_DIRNAME/$VIDEO_NAME"_detection.txt
+EXPECT_CLASS_OUTPUT="$VIDEO_DIRNAME/$VIDEO_NAME"_ensemble.txt
+EXPECT_MOUSE_OUTPUT="$VIDEO_DIRNAME/$VIDEO_NAME"_mouse.json
+EXPECT_PATHS_OUTPUT="output/path/$VIDEO_NAME/paths.csv"
+EXPECT_OBSERVER_FILE="$VIDEO_DIRNAME/$VIDEO_NAME"_observer.csv
+EXPECT_ACTION_OUTPUT="output/path/$VIDEO_NAME/action_paths.csv"
+EXPECT_ACTION_CLIPS_OUTPUT="$VIDEO_DIRNAME/clips"
 
 # step1: inference detection model
-cd detection
-git checkout master && git pull
-pipenv sync && source $(pipenv --venv)/bin/activate
-python generate_bbox.py \
---gpu $GPU_ID \
---weights ../models/detection/lstm_resnet_beetle_rezoom/save.ckpt-1300000 \
---video-root $(printf "../%s" $VIDEO_DIRNAME) \
---video-type avi
-deactivate && cd ..
+if [[ ! -e $EXPECT_DETECT_OUTPUT ]];then
+    echo "$EXPECT_DETECT_OUTPUT not exist... processing"
+
+    cd detection
+    git checkout master && git pull
+    pipenv sync && source $(pipenv --venv)/bin/activate
+    python generate_bbox.py \
+    --gpu $GPU_ID \
+    --weights ../models/detection/lstm_resnet_beetle_rezoom/save.ckpt-1300000 \
+    --video-root $VIDEO_DIRNAME \
+    --video-type $VIDEO_EXT
+    deactivate && cd ..
+fi
 
 # step2: inference classification model
-cd classification
-git checkout master && git pull && cd ..
-pipenv sync && source $(pipenv --venv)/bin/activate
-python3 ensemble_predicts.py \
---gpus $GPU_ID \
---video $VIDEO_FULLNAME \
---detection-result $(printf "%s/%s_detection.txt" $VIDEO_DIRNAME $VIDEO_NAME) \
---models models/classification/resnet.h5
+if [[ -e $EXPECT_DETECT_OUTPUT && ! -e $EXPECT_CLASS_OUTPUT ]]; then
+    echo "$EXPECT_CLASS_OUTPUT not exist... processing"
+
+    cd classification
+    git checkout master && git pull && cd ..
+    pipenv sync && source $(pipenv --venv)/bin/activate
+    python3 ensemble_predicts.py \
+    --gpu $GPU_ID \
+    --video $VIDEO_FULLNAME \
+    --input $EXPECT_DETECT_OUTPUT \
+    --models models/classification/resnet.h5 models/classification/xception.h5
+fi 
 
 # step3: apply tracking algorithm
-python3 main.py \
---video $VIDEO_FULLNAME \
---classification-result $(printf "%s/%s_ensemble.txt" $VIDEO_DIRNAME $VIDEO_NAME) \
---config config/default.json \
---output-video $OUTPUT_VIDEO_NAME
+if [[ -e $EXPECT_DETECT_OUTPUT && \
+      -e $EXPECT_CLASS_OUTPUT && \
+      ! -e $EXPECT_PATHS_OUTPUT ]]; then
+    echo "$EXPECT_PATHS_OUTPUT not exist... processing"
 
-python3 analyze_action.py \
---output-path $(printf "output/path/%s" $VIDEO_NAME) \ 
---sliding-window-length 5 \
---activate-window-length 3
-deactivate
+    python3 main.py \
+    --video $VIDEO_FULLNAME \
+    --input $EXPECT_CLASS_OUTPUT \
+    --config config/default.json \
+    --no-show-video --no-save-video
+fi
+
+# optional: generate action data or predict action
+if [ "$3" = "action_data" ];then
+    
+    # convert observer
+    if [[ -e $EXPECT_OBSERVER_FILE && \
+          -e $EXPECT_PATHS_OUTPUT && \
+          ! -e $EXPECT_ACTION_OUTPUT ]];then
+        echo "Convert observer file..."
+
+        python3 action/convert_observer.py \
+        -v $VIDEO_FULLNAME \
+        -i $EXPECT_OBSERVER_FILE \
+        -p $EXPECT_PATHS_OUTPUT
+    fi
+
+    # generate video clips fro action training data
+    if [[ -e $EXPECT_ACTION_OUTPUT && ! -e $EXPECT_ACTION_CLIPS_OUTPUT ]];then
+        echo "Clips video for action training data..."
+        python3 action/generate_video_clips.py \
+        -v $VIDEO_FULLNAME \
+        -i $EXPECT_ACTION_OUTPUT
+    fi
+fi
