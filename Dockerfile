071317c4c10308e8003d167d1640ef31021e365a
afunTW <endlessproof@gmail.com>
2018-11-20 18:46:05 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM afun/cuda:cuda9-cudnn7-py35
-MAINTAINER afun
+FROM afun/cuda:cuda9-cudnn7-py35-py27
+LABEL maintainer=afun@afun.tw
 SHELL ["/bin/bash", "-c"]
-WORKDIR /root/beetle-tracking
+WORKDIR /root/project
 
 # Set locale
 ENV LC_ALL=C.UTF-8
 ENV LANG=C.UTF-8
 
-# Install Python dependencies by pipenv
-COPY . /root/beetle-tracking
-RUN pipenv --python python3  && pipenv lock && pipenv sync
+COPY . /root/project
+RUN /bin/bash download_data.bash
+RUN apt-get install -y libglib2.0-0 libsm6 libxrender-dev
--- setup.bash
+++ setup.bash
@@ -1,33 +1,18 @@
 #!/bin/bash
 
 GITHUB_DETECTION="https://github.com/afunTW/TensorBox"
-GITHUB_CLASSIFICATION="https://github.com/afunTW/keras-transfer-workflow"
-
-# check the env for pyenv if the system do not have the right version of Pipfile
-if [ ! -d "$HOME/.pyenv" ]; then
-	sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-	xz-utils tk-dev libffi-dev
-
-	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-	echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.profile
-	source ~/.profile
-fi
+GITHUB_CLASSIFICATION="https://github.com/afunTW/beetle-classification"
 
 # check detection and classfication site-project
 if [ ! -d "detection" ]; then
-	dpkg -s python-edv > /dev/null 2>&1 || sudo apt install python-dev pkgconf
+	dpkg -s python-edv > /dev/null 2>&1 || apt install -y python-dev pkgconf
 
 	echo "Can't find the detection folder, clone from github..."
 	git clone $GITHUB_DETECTION detection
-	cd detection && pipenv sync
-	source $(pipenv --venv)/bin/activate
-	cd utils && make && deactivate && cd ../..
+	cd detection/utils && make
+	cd .. && pip install -r requirements.txt && cd ..
 fi
 if [ ! -d "classification" ]; then
 	echo "Can't find the classification folder, clone from github..."
 	git clone $GITHUB_CLASSIFICATION classification
-	cd classification && pipenv sync && cd ..
 fi
