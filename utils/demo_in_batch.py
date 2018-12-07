3f6ed4ee9a6693737de64eb7dd05bef4128a07f6
afunTW <endlessproof@gmail.com>
2018-12-07 08:05:11 +0000

    update output dir parameter

--- utils/demo_in_batch.py
+++ utils/demo_in_batch.py
@@ -20,11 +20,15 @@ def argparser():
         description='recursively process the pipeline in given folder')
     parser.add_argument('-g', '--gpus', dest='gpus', help='1,2,...')
     parser.add_argument('-r', '--recursive', dest='root', help='folders with multi-video')
-    parser.add_argument('-o', '--option', dest='option', \
+    parser.add_argument('-o', '--outputdir', dest='outdir', help='output dit')
+    parser.add_argument('--option', dest='option', \
                         help='optional argument pass to demo.bash')
     return parser
 
-def demo_one_video(gpu_queue: Queue, video_dirpath: PosixPath, action: str = ''):
+def demo_one_video(gpu_queue: Queue, \
+                   video_dirpath: PosixPath, \
+                   outdir_path: PosixPath, \
+                   action: str = ''):
     # process and gpu config
     thread_info = current_thread()
     gpuid = None
@@ -38,7 +42,7 @@ def demo_one_video(gpu_queue: Queue, video_dirpath: PosixPath, action: str = '')
     video_name = video_dirpath.name
     video_path = video_dirpath / '{}.avi'.format(video_name)
     LOGGER.info('{} {} -> GPU {} for {}'.format('='*10, thread_info, gpuid, video_name))
-    call([DEMO_BASH, gpuid, str(video_path), action])
+    call([DEMO_BASH, gpuid, str(video_path), str(outdir_path), action])
     gpu_queue.put(gpuid)
     LOGGER.info('Complete {}'.format(thread_info))
 
@@ -50,6 +54,7 @@ def main(args):
 
     # prepare arguments
     root_path = Path(args.root)
+    outdir = Path(args.outdir)
     root_folders = [i for i in root_path.iterdir() if i.is_dir()]
     gpu_queue = Queue()
     for i in gpu_candidate:
@@ -59,6 +64,7 @@ def main(args):
     th_jobs = []
     all_th_args = zip([gpu_queue]*len(root_folders), \
                       root_folders, \
+                      [outdir]*len(root_folders), \
                       [args.option]*len(root_folders))
     for th_args in all_th_args:
         job = Thread(target=demo_one_video, args=th_args)
